5b8ecc089f6abcec63b08183ff02746291a8f2dc
DeveloperSky95 <haneulgim95@gmail.com>
2026-08-13 16:37:23 +0900

    createUser 작성 완료

--- src/main/java/com/lineacademy/fridgemanagerspring/domain/user/User.java
+++ src/main/java/com/lineacademy/fridgemanagerspring/domain/user/User.java
@@ -9,6 +9,7 @@ import com.lineacademy.fridgemanagerspring.domain.shoppinglist.ShoppingList;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -37,14 +38,14 @@ public class User extends BaseTimeEntity {
     private String email;
 
     @Column()
-    private LocalDateTime birthdate;
+    private LocalDate birthdate;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private RoleType role = RoleType.USER;
 
     @Builder
-    private User(String nickname, String password, String email, LocalDateTime birthdate, RoleType role) {
+    private User(String nickname, String password, String email, LocalDate birthdate, RoleType role) {
         this.nickname = nickname;
         this.password = password;
         this.email = email;
@@ -56,7 +57,7 @@ public class User extends BaseTimeEntity {
         this.nickname = nickname;
     }
 
-    public void updateBirthdate(LocalDateTime birthdate) {
+    public void updateBirthdate(LocalDate birthdate) {
         this.birthdate = birthdate;
     }
 
