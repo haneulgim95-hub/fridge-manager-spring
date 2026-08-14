4001afae04ab954bc6cd906e073ba27b5986419a
DeveloperSky95 <haneulgim95@gmail.com>
2026-08-14 16:50:30 +0900

    createNotice 작성 (<- 관리자전용)

--- src/main/java/com/lineacademy/fridgemanagerspring/service/NoticeService.java
+++ src/main/java/com/lineacademy/fridgemanagerspring/service/NoticeService.java
@@ -1,7 +1,9 @@
 package com.lineacademy.fridgemanagerspring.service;
 
 import com.lineacademy.fridgemanagerspring.domain.notice.Notice;
+import com.lineacademy.fridgemanagerspring.dto.notice.request.NoticeRequest;
 import com.lineacademy.fridgemanagerspring.repository.NoticeRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -24,5 +26,15 @@ public class NoticeService {
     public Notice getNoticeById(Long noticeId) {
         return noticeRepository.findById(noticeId)
                 .orElseThrow(() -> new RuntimeException("NOT_FOUND_NOTICE"));
+    }
+
+    @Transactional
+    public Notice createNotice(NoticeRequest request) {
+        Notice notice = Notice.builder()
+                .title(request.getTitle())
+                .content(request.getContent())
+                .build();
+
+        return noticeRepository.save(notice);
     }
 }
